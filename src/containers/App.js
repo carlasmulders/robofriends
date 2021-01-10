a917c6e70fb2fb001fbab5c215053797f93c1dd4
csmulders <c.smulders@abt.eu>
2021-01-10 13:55:28 +0100

    Use react hooks in app.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,48 +1,41 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import './App.css';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 
-class App extends Component {
-	constructor(){
-		super()
-		this.state = {
-			robots: [], 
-			searchfield: ''
-		}
-	}
+function App() {
+	const [robots, setRobots] = useState([]);
+	const [searchfield, setSearchfield] = useState('');
 
-	componentDidMount(){
+	useEffect(() => {
 		fetch('https://jsonplaceholder.typicode.com/users')
-		.then(response => response.json())
-		.then(robots => this.setState({robots: robots}));
+	 	.then(response => response.json())
+	 	.then(users => setRobots(users));
+	}, [])
+
+	const onSearchChange = (event) => {
+		setSearchfield(event.target.value);
 	}
 
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value });
-	}
-
-	render() {
-		const {robots, searchfield} = this.state;
-		const filteredRobots = robots.filter(robot => {
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-		})
-		return !robots.length ? 
-		<h1>Loading</h1> :	
-		(
-			<div className='tc'>
-				<h1 className='f1' >RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange}/>
-				<Scroll>
-					<ErrorBoundary>
-						<CardList robots = {filteredRobots}/>
-					</ErrorBoundary>
-				</Scroll>
-			</div>
-		);
-	}	
+	const filteredRobots = robots.filter(robot => {
+		return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+	})
+	return !robots.length ? 
+	<h1>Loading</h1> :	
+	(
+		<div className='tc'>
+			<h1 className='f1' >RoboFriends</h1>
+			<SearchBox searchChange={onSearchChange}/>
+			<Scroll>
+				<ErrorBoundary>
+					<CardList robots = {filteredRobots}/>
+				</ErrorBoundary>
+			</Scroll>
+		</div>
+	);
+		
 }
 
 export default App;
